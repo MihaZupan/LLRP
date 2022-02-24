3ec5b2636f39e4f38ed8103890e7ff865059e58f
Miha Zupan <mihazupan.zupan1@gmail.com>
2022-02-24 01:33:15 +0100

    Move HttpClientConfiguration to a separate file

--- LLRP/HttpClientApplication.cs
+++ LLRP/HttpClientApplication.cs
@@ -5,46 +5,6 @@ using System.Net.Http.Headers;
 
 namespace LLRP
 {
-    internal static class HttpClientConfiguration
-    {
-        public static bool ShareClients = false;
-        public static bool RoundRobin = false;
-        public static HttpMessageInvoker[] SharedClients = null!;
-        private static int _count;
-
-        public static HttpMessageInvoker CreateClient()
-        {
-            return new HttpMessageInvoker(new SocketsHttpHandler()
-            {
-                UseProxy = false,
-                AllowAutoRedirect = false,
-                AutomaticDecompression = DecompressionMethods.None,
-                UseCookies = false,
-                ActivityHeadersPropagator = null,
-                PooledConnectionIdleTimeout = TimeSpan.FromDays(1), // Avoid the cleaning timer executing during the benchmark
-            });
-        }
-
-        public static HttpMessageInvoker? GetFixedClient()
-        {
-            if (ShareClients)
-            {
-                if (RoundRobin)
-                {
-                    return null;
-                }
-                else
-                {
-                    return SharedClients[Interlocked.Increment(ref _count) % SharedClients.Length];
-                }
-            }
-            else
-            {
-                return CreateClient();
-            }
-        }
-    }
-
     internal sealed partial class HttpClientApplication : ApplicationBase<HttpClientApplication>
     {
         private readonly HttpMessageInvoker? _client;
---
+++ LLRP/HttpClientConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http;
+
+namespace LLRP
+{
+    internal static class HttpClientConfiguration
+    {
+        public static bool ShareClients = false;
+        public static bool RoundRobin = false;
+        public static HttpMessageInvoker[] SharedClients = null!;
+        private static int _count;
+
+        public static HttpMessageInvoker CreateClient()
+        {
+            return new HttpMessageInvoker(new SocketsHttpHandler()
+            {
+                UseProxy = false,
+                AllowAutoRedirect = false,
+                AutomaticDecompression = DecompressionMethods.None,
+                UseCookies = false,
+                ActivityHeadersPropagator = null,
+                PooledConnectionIdleTimeout = TimeSpan.FromDays(1), // Avoid the cleaning timer executing during the benchmark
+            });
+        }
+
+        public static HttpMessageInvoker? GetFixedClient()
+        {
+            if (ShareClients)
+            {
+                if (RoundRobin)
+                {
+                    return null;
+                }
+                else
+                {
+                    return SharedClients[Interlocked.Increment(ref _count) % SharedClients.Length];
+                }
+            }
+            else
+            {
+                return CreateClient();
+            }
+        }
+    }
+}
